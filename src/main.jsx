f000ac082ddd043915eb5f45917259453cbe5d2a
lom <kutmanamangeldiev@Kutmans-MacBook-Pro.local>
2024-05-13 23:27:12 +0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./routes/RootLayout.jsx";
+import Posts from "./routes/Posts.jsx";
+import NewPost from "./routes/NewPost.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        children: [{ path: "/create-post", element: <NewPost /> }],
+      },
+      ,
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
